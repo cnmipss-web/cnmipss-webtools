4714cb75edde05850cf2cd3f3d7555e0b3572138
Tyler Collins <tjscollins@gmail.com>
2017-07-10 14:36:27 +1000

    Get datomic uri from ENV variable

--- src/clj/certification_db/db.clj
+++ src/clj/certification_db/db.clj
@@ -1,12 +1,13 @@
 (ns certification-db.db
   (:require [datomic.api :as d]
             [datomic-schema.schema :as s]
+            [certification-db.config :refer [env]]
             [mount.core :as mount]))
 
 (mount/defstate ^{:on-reload :noop}
   conn
   :start
-  (d/connect "datomic:dev://localhost:4334/hello")
+  (d/connect (:datomic-uri env))
   :stop
   (d/shutdown false))
 
@@ -34,20 +35,27 @@
 
 (defn- init-schema
   []
-  (d/transact conn (s/generate-schema db-schema)))
+  (d/transact conn (s/generate-schema db-schema))) 
 
 
 (defn- create-user!
   [user]
   (d/transact conn [user]))
 
-(def ^:private email->id '[:find ?e :in $ ?email :where [?e :user/email ?email]])
-(def ^:private email->type '[:find ?type :in $ ?email :where [?e :user/email ?email] [?e :user/type ?type]])
-(def ^:private email->token '[:find ?token :in $ ?email :where [?e :user/email ?email] [?e :user/token ?token]])
+(def ^:private email->id '[:find ?e :in $ ?email
+                           :where [?e :user/email ?email]])
+
+(def ^:private email->type '[:find ?type :in $ ?email
+                             :where [?e :user/email ?email] [?e :user/type ?type]])
+
+(def ^:private email->token '[:find ?token :in $ ?email
+                              :where [?e :user/email ?email] [?e :user/token ?token]])
+
 (def ^:private email->user '[:find ?e ?email ?name ?type :in $ ?email
                              :where [?e :user/email ?email]
                              [?e :user/name ?name]
                              [?e :user/type ?type]])
+
 (def ^:private cert-no->cert '[:find ?ln ?fn ?mi ?type ?cert-no ?s-d ?e-d :in $ ?cert-no
                                :where
                                [?e :certification/cert-no ?cert-no]
@@ -57,6 +65,7 @@
                                [?e :certification/type ?type]
                                [?e :certification/start-date ?s-d]
                                [?e :certification/expiry-date ?e-d]])
+
 (def ^:private all-certs '[:find ?ln ?fn ?mi ?type ?cert-no ?s-d ?e-d
                                :where
                                [?e :certification/cert-no ?cert-no]
