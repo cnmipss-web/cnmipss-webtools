c90ff17b6360882866500623a22d6635277f15a3
Tyler Collins <tjscollins@gmail.com>
2017-08-09 05:49:44 +0000

    Added sql migrations and queries for procurement addenda

---
+++ resources/sql/procurement-addenda-queries.sql
@@ -0,0 +1,20 @@
+-- :name create-addendum! :! :1
+-- :doc Creates a new addendum to an existing rfp/ifb
+insert into procurement_addenda
+(id, file_link, rfp_id, ifb_id)
+values (:id, :file_link, :rfp_id, :ifb_id);
+
+-- :name delete-addendum! :! :1
+-- :doc Deletes an addendum from the database
+delete from procurement_addenda
+where id = :id;
+
+-- :name get-rfp-addenda :? :*
+-- :doc Retrieve addenda with matching :rfp_no
+select * from procurement_addenda
+where rfp_id = :rfp_id;
+
+-- :name get-ifb-addenda :? :*
+-- :doc Retrieve addenda with matching ifb_no
+select * from procurement_addenda
+where ifb_id = :ifb_id;
