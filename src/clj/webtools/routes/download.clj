05afb157b61d0df2946c718eb00517ae69a4cf59
Tyler Collins <tjscollins@gmail.com>
2017-08-14 21:55:28 +0000

    Download routes

---
+++ src/clj/webtools/routes/download.clj
@@ -0,0 +1,43 @@
+(ns webtools.routes.download
+  (:require [compojure.core :refer [defroutes GET]]
+            [ring.util.http-response :as response]
+            [clojure.data.json :as json]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [webtools.db.core :as db]
+            [webtools.email :as email]
+            [webtools.config :refer [env]]
+            [webtools.util :refer :all]
+            [webtools.json :refer :all]
+            [webtools.layout :refer [error-page]]
+            [webtools.constants :refer [max-cookie-age]  :as const]
+            [webtools.wordpress-api :as wp]
+            [clojure.tools.logging :as log]))
+
+
+(defn capitalize-words 
+  "Capitalize every word in a string"
+  [s]
+  (->> (clojure.string/split (str s) #"\b") 
+       (map clojure.string/capitalize)
+       clojure.string/join))
+
+(defroutes download-routes
+  (GET "/download/subscribers/:id" [id]
+       (let [uuid (java.util.UUID/fromString id)
+             subscriptions (->> (db/get-subscriptions {:rfp_id uuid :ifb_id uuid})
+                               (mapv #(assoc % :telephone (format-tel-num (:telephone %)))))
+             columns (->> subscriptions
+                          first
+                          keys
+                          (mapv name)
+                          (mapv #(capitalize-words (clojure.string/replace % #"_" " "))))
+             rows (mapv vals subscriptions)
+             data (->> (cons columns rows)
+                       (mapv #(drop 4 %)))]
+         (with-open [temp (io/writer "temp.csv")]
+           (csv/write-csv temp data))
+         (-> (response/file-response "temp.csv")
+             (response/header "Content-Type" "text/csv")
+             (response/header "Content-Disposition"
+                              (str "attachment;filename=" "subscribers.csv"))))))
