056db031c6a9e4ec41d8eefc81d72504fa049c84
Tyler Collins <tjscollins@gmail.com>
2017-10-23 01:55:06 +0000

    Fixed phone number seeds to pass validation

--- resources/sql/test-seed.sql
+++ resources/sql/test-seed.sql
@@ -123,10 +123,10 @@ insert into procurement_subscriptions (id, proc_id, subscription_number, company
 values ('7cb66ca0-8741-4067-a9a3-993d93c42e69', 'd2b4e97c-5d7c-4ccd-8fae-a27a27c863e3', 1, 'Complected Construction', 'Juan Tzu', 'j.tzu@complect.thisdomainisfake', 16705505550);
 
 insert into procurement_subscriptions (id, proc_id, subscription_number, company_name, contact_person, email, telephone)
-values ('8eb76ca0-8751-4561-a9a3-903d93a42f65', 'd2b4e97c-5d7c-4ccd-8fae-a27a27c863e3', 2, 'Testing Subscriptions', 'Tyler Collins', 'tyler.collins@cnmipss.org', 456123);
+values ('8eb76ca0-8751-4561-a9a3-903d93a42f65', 'd2b4e97c-5d7c-4ccd-8fae-a27a27c863e3', 2, 'Testing Subscriptions', 'Tyler Collins', 'tyler.collins@cnmipss.org', 7456123);
 
 insert into procurement_subscriptions (id, proc_id, subscription_number, company_name, contact_person, email, telephone)
-values ('2af76ca0-3711-7551-a9a3-903d93a42f65', 'cf82deed-c84f-446c-a3f0-0d826428ddbd', 2, 'Testing Subscriptions', 'Tyler Collins', 'tyler.collins@cnmipss.org', 456123);
+values ('2af76ca0-3711-7551-a9a3-903d93a42f65', 'cf82deed-c84f-446c-a3f0-0d826428ddbd', 2, 'Testing Subscriptions', 'Tyler Collins', 'tyler.collins@cnmipss.org', 8456123);
 
 -- :name clear-subscriptions :! :n
 -- :doc clear the db of all procurement subscriptions
