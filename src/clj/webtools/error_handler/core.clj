9380665c80f272e13eb5e3e4b70df996286baf2e
Tyler Collins <tjscollins@gmail.com>
2017-12-17 22:46:27 +0000

    Protocols for handling errors and generating error codes and messages in user-friendly language

---
+++ src/clj/webtools/error_handler/core.clj
@@ -0,0 +1,17 @@
+(ns webtools.error-handler.core
+  (:require [webtools.error-handler.sql :as sql-error]))
+
+(defprotocol HandleErrors
+  (code [x] "Return an error-code based on the type of error in terms of our data model.")
+  (msg [x] "A human friendly error message based on the type of error in terms of our data model"))
+
+(extend-protocol HandleErrors
+  java.sql.SQLException
+  (code [err]
+    (sql-error/code err))
+  (msg [err]
+    (sql-error/msg err))
+  
+  Exception
+  (code [err] "unknown-err")
+  (msg [err] "An unknown error occurred.  Please contact the developer."))
--- src/clj/webtools/error_handler/sql.clj
+++ src/clj/webtools/error_handler/sql.clj
@@ -4,25 +4,31 @@
 ;;Regexes to identify source of sql errors
 (def open_date_null #"null value in column \"open_date\" violates not-null constraint")
 
-
 (defprotocol ReportError
   (code [x] "Return an error-code based on the type of error in terms of our data model.")
   (msg [x]  "A human friendly error message based on the type of error in terms of our data model"))
 
-(defrecord malformed-date [error]
+(defrecord malformed-date-error [error]
   ReportError
   (code [x] "bad-date")
   (msg [x]  "One of the required dates is incorrectly formatted."))
 
 (defrecord unknown-error [error]
   ReportError
-  (code [x] "unknown")
-  (msg  [x] "Unknown error.  Please contact the developer."))
+  (code [x] "unknown-sql")
+  (msg  [x] "Unknown SQL error.  Please contact the developer."))
 
-(defn type
+(defn err-type
   "Determine the type of error in terms of our data model"
   [error]
   (let [message (.getMessage error)]
     (cond
-      (re-seq open_date_null message) (->malformed-date error)
+      (re-seq open_date_null message) (->malformed-date-error error)
       "" (->unknown-error error))))
+
+(extend-protocol ReportError
+  java.sql.BatchUpdateException
+  (code [err]
+    (code (err-type err)))
+  (msg [err]
+    (msg (err-type err))))
