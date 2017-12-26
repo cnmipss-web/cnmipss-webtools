f5a6ddeee3feaee5a203a17097faa65179f29598
Tyler Collins <tjscollins@gmail.com>
2017-12-26 14:56:40 +1000

    HROTests class declarations

---
+++ test/selenium/test_hro.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+from settings import WT_ID, WT_PWD, WT_URL
+from login import login
+
+
+class HROTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_upload_form(self):
+        pass
+
+    def test_search_form(self):
+        pass
+
+    def test_table_renders(self):
+        pass
+
+    def test_edit_modal(self):
+        pass
