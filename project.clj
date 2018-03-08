b30d2018278ab712d8b926e9d37e6e3fb8bf82d4
Tyler Collins <tjscollins@gmail.com>
2018-03-08 13:00:09 -1000

    Update dependencies and get lein doo working

--- project.clj
+++ project.clj
@@ -3,59 +3,59 @@
   :description "Internal Web Application for various CNMI PSS Webtools"
   :url "http://www.cnmipss.org/webtools/"
 
-  :dependencies [[cheshire "5.7.1"]
+  :dependencies [[cheshire "5.8.0"]
                  [cider/cider-nrepl "0.16.0"]
                  [clj-fuzzy "0.4.1"]
-                 [clj-http "3.6.1"]
-                 [clj-time "0.14.0"]
-                 [cljs-ajax "0.6.0"]
+                 [clj-http "3.7.0"]
+                 [clj-time "0.14.2"]
+                 [cljs-ajax "0.7.3"]
                  [cljsjs/jquery "3.2.1-0"]
                  [clojusc/friend-oauth2 "0.2.0"]
-                 [com.andrewmcveigh/cljs-time "0.5.0"]
+                 [com.andrewmcveigh/cljs-time "0.5.2"]
                  [com.cemerick/friend "0.2.3"]
                  [com.cemerick/url "0.1.1"]
                  [com.draines/postal "2.0.2"]
-                 [com.layerware/hugsql "0.4.7"]
+                 [com.layerware/hugsql "0.4.8"]
                  [compojure "1.6.0"]
-                 [conman "0.6.4"]
-                 [cprop "0.1.10"]
+                 [conman "0.7.6"]
+                 [cprop "0.1.11"]
                  [criterium "0.4.4"]
                  [dk.ative/docjure "1.12.0"]
-                 [funcool/struct "1.0.0"]
+                 [funcool/struct "1.2.0"]
                  [hiccup "1.0.5"]
                  [jarohen/chime "0.2.2"]
-                 [klang "0.5.7"]
-                 [luminus-immutant "0.2.3"]
-                 [luminus-migrations "0.3.9"]
+                 [klang "0.5.13"]
+                 [luminus-immutant "0.2.4"]
+                 [luminus-migrations "0.5.0"]
                  [luminus-nrepl "0.1.4"]
                  [luminus/ring-ttl-session "0.3.2"]
-                 [markdown-clj "0.9.99"]
-                 [metosin/muuntaja "0.3.1"]
+                 [markdown-clj "1.0.2"]
+                 [metosin/muuntaja "0.5.0"]
                  [metosin/ring-http-response "0.9.0"]
-                 [mount "0.1.11"]
-                 [org.apache.pdfbox/pdfbox "2.0.6"]
+                 [mount "0.1.12"]
+                 [org.apache.pdfbox/pdfbox "2.0.8"]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.671" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.145" :scope "provided"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/test.check "0.10.0-alpha2"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.flatland/useful "0.11.5"]
-                 [org.postgresql/postgresql "42.0.0"]
-                 [org.webjars.bower/tether "1.4.0"]
+                 [org.postgresql/postgresql "42.2.1"]
+                 [org.webjars.bower/tether "1.4.3"]
                  [org.webjars/bootstrap "4.0.0-alpha.5"]
                  [org.webjars/font-awesome "4.7.0"]
-                 [re-frame "0.9.4"]
+                 [re-frame "0.10.5"]
                  [reagent "0.7.0"]
-                 [reagent-utils "0.2.1"]
+                 [reagent-utils "0.3.1"]
                  [ring-cors "0.1.11"]
                  [ring-webjars "0.2.0"]
-                 [ring/ring-core "1.6.1"]
-                 [ring/ring-defaults "0.3.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-defaults "0.3.1"]
                  [ring/ring-json "0.4.0"]
-                 [ring/ring-spec "0.0.3"]
+                 [ring/ring-spec "0.0.4"]
                  [secretary "1.2.3"]
-                 [selmer "1.10.8"]
+                 [selmer "1.11.7"]
                  [slester/ring-browser-caching "0.1.1"]
                  [stuarth/clj-oauth2 "0.3.2"]
                  [try-let "1.1.0"]]
@@ -78,8 +78,7 @@
             [lein-immutant "2.1.0"]
             [lein-sassc "0.10.4"]
             [migratus-lein "0.4.9"]
-            [lein-doo "0.1.7"]
-            [jonase/eastwood "0.2.5"]]
+            [lein-doo "0.1.7"]]
 
   :sassc
   [{:src "resources/scss/screen.scss"
@@ -125,21 +124,21 @@
    :dev           [:project/dev :profiles/dev]
    :test          [:project/dev :project/test :profiles/test]
 
-   :project/dev  {:dependencies [[binaryage/devtools "0.9.4"]
+   :project/dev  {:dependencies [[binaryage/devtools "0.9.9"]
                                  [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
-                                 [circleci/bond "0.3.0"]
+                                 [circleci/bond "0.3.1"]
                                  [com.cemerick/piggieback "0.2.2"]
-                                 [doo "0.1.7"]
-                                 [figwheel-sidecar "0.5.11"]
-                                 [pjstadig/humane-test-output "0.8.2"]
-                                 [prone "1.1.4"]
-                                 [ring/ring-devel "1.6.1"]
-                                 [ring/ring-mock "0.3.1"]]
+                                 [doo "0.1.8"]
+                                 [figwheel-sidecar "0.5.15"]
+                                 [pjstadig/humane-test-output "0.8.3"]
+                                 [prone "1.5.0"]
+                                 [ring/ring-devel "1.6.3"]
+                                 [ring/ring-mock "0.3.2"]]
 
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.19.0"]
-                                 [lein-doo "0.1.7"]
-                                 [lein-figwheel "0.5.11"]
-                                 [org.clojure/clojurescript "1.9.671"]
+                                 [lein-doo "0.1.8"]
+                                 [lein-figwheel "0.5.15"]
+                                 [org.clojure/clojurescript "1.10.145"]
                                  [lein-cloverage "1.0.9"]]
                   
                   :cljsbuild
@@ -162,11 +161,11 @@
                      :compiler
                      {:output-to "target/test.js"
                       :main "webtools.doo-runner"
-                      :optimizations :whitespace
+                      :optimizations :none
                       :pretty-print true}}}}
                   
-                  :doo {:alias {:default [:phantom] 
-                                :all [:chrome :firefox :safari]}
+                  :doo {:alias {:default [:chrome] 
+                                :all [:chrome :firefox]}
                         :build "test"}
                   :source-paths ["env/dev/clj"]
                   :resource-paths ["env/dev/resources"]
@@ -183,6 +182,9 @@
                      {:output-to "target/test.js"
                       :main "webtools.doo-runner"
                       :optimizations :whitespace
-                      :pretty-print true}}}}}
+                      :pretty-print true}}}}
+                  :doo {:alias {:default [:phantom] 
+                                :all [:chrome :firefox :safari]}
+                        :build "test"}}
    :profiles/dev {}
    :profiles/test {}})
