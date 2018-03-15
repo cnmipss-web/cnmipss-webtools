e8b0166a61a199b70f9984421102b4d5e3898daf
Tyler Collins <tjscollins@gmail.com>
2018-03-14 14:58:38 -1000

    Add a query to get-oldest-fns-nap
    Needed to query the oldest record before deleting it in order to
    delete files associated with the record.

--- resources/sql/fns-nap-queries.sql
+++ resources/sql/fns-nap-queries.sql
@@ -1,16 +1,21 @@
 -- :name create-fns-nap! :! :n
 -- :doc creates a new record of matched fns-nap records
 INSERT INTO fns_nap
-(date_created, fns_file_link, nap_file_link, matched_file_link)
-VALUES ( SELECT current_date(), :fns_file_link, :nap_file_link, :matched_file_link)
+(id, date_created, fns_file_link, nap_file_link, matched_file_link)
+VALUES (:id, CURRENT_TIMESTAMP, :fns_file_link, :nap_file_link, :matched_file_link)
+
+-- :name get-oldest-fns-nap :? :1
+-- :doc retrieve the oldest record from fns_nap
+SELECT * FROM fns_nap
+WHERE date_created=(SELECT MIN(date_created) FROM fns_nap)
 
 -- :name drop-oldest-fns-nap! :! :n
 -- :doc deletes the oldest record from fns_nap
-DELETE FROM fns_nap 
-WHERE data_created IS NOT NULL 
-ORDER BY date_created DESC 
-LIMIT 1
+DELETE FROM fns_nap
+WHERE date_created=( SELECT MIN(date_created) FROM fns_nap )
+
 
 -- :name get-all-fns-nap :? :*
 -- :doc retrieve the list of stored fns-nap registrations
 SELECT * FROM fns_nap
+ORDER BY date_created DESC
