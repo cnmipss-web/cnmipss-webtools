da6c8856cdc78e2c9daa98f2786df38fa240ffcb
Tyler Collins <tjscollins@gmail.com>
2018-03-20 10:53:22 -1000

    Create handlers for custom ex-info exceptions

---
+++ src/clj/webtools/exceptions.clj
@@ -0,0 +1,49 @@
+(ns webtools.exceptions
+  (:import [java.io StringWriter PrintWriter]))
+
+(defn exception->string [exception]
+  (let [sw (java.io.StringWriter.)
+        pw (java.io.PrintWriter. sw)]
+    (binding [*out* pw]
+      (println exception))
+    (.toString sw)))
+
+(defprotocol generate-ex-info
+  (wrap-ex [ex data] "Wrap a Java runtime Exception with clojure.lang.ExceptionInfo"))
+
+(defn null-pointer
+  "Generate a custom null-pointer exception using ex-info.  Accepts a map
+  with the following keys:
+
+    :msg   -- A message describing the source of the exception
+    :data  -- A map containing data relevant to debugging the exception
+    :cause -- A throwable object which is being wrapped by ex-info.
+
+  At least one of :msg or :cause must be some?"
+  [{:keys [msg data cause]}]
+  (let [default-data {:msg (if (some? cause) (.getMessage cause))
+                      :type NullPointerException}]
+    (ex-info msg (merge default-data data) cause)))
+
+(defn illegal-argument
+  "Generate a custom illegal-argument exception using ex-info.  Accepts a map
+  with the following keys:
+
+    :msg   -- A message describing the source of the exception
+    :data  -- A map containing data relevant to debugging the exception
+    :cause -- A throwable object which is being wrapped by ex-info.
+
+  At least one of :msg or :cause must be some?"
+  [{:keys [msg data cause]}]
+  (let [default-data {:msg (if (some? cause) (.getMessage cause))
+                      :type IllegalArgumentException}]
+    (ex-info msg (merge default-data data) cause)))
+
+(extend-protocol generate-ex-info
+  IllegalArgumentException
+  (wrap-ex [ex data]
+    (illegal-argument (merge data {:cause ex})))
+  
+  NullPointerException
+  (wrap-ex [ex data]
+    (null-pointer (merge data {:cause ex}))))
