60a06c74416a7ac0722bad649e339c2efc47feb1
Tyler Collins <tjscollins@gmail.com>
2018-04-08 17:30:11 -1000

    Move custom json encoders to webtools.json

--- src/clj/webtools/db/core.clj
+++ src/clj/webtools/db/core.clj
@@ -65,14 +65,6 @@
         (.setObject stmt idx (.createArrayOf conn elem-type (to-array v)))
         (.setObject stmt idx (to-pg-json v))))))
 
-(add-encoder org.joda.time.DateTime
-             (fn [date jg]
-               (if (and (zero? (t/hour date))
-                        (zero? (t/minute date))
-                        (zero? (t/second date)))
-                 (.writeString jg (util-dates/print-date date))
-                 (.writeString jg (util-dates/print-date-at-time date)))))
-
 (extend-protocol jdbc/ISQLValue
   clojure.lang.Keyword
   (sql-value [value] (name value))
--- src/clj/webtools/json.clj
+++ src/clj/webtools/json.clj
@@ -1,6 +1,28 @@
 (ns webtools.json
   (:require [cheshire.core :as json]
-            [clojure.walk :as walk]))
+            [cheshire.generate :refer [add-encoder]]
+            [clojure.walk :as walk]
+            [clj-time.coerce :as c]
+            [clj-time.core :as t]
+            [webtools.util.dates :as util-dates]
+            ))
+
+
+;; Create custom JSON encoders that we will use
+(add-encoder org.joda.time.DateTime
+             (fn [date jg]
+               (if (and (zero? (t/hour date))
+                        (zero? (t/minute date))
+                        (zero? (t/second date)))
+                 (.writeString jg (util-dates/print-date date))
+                 (.writeString jg (util-dates/print-date-at-time date)))))
+
+(add-encoder java.lang.Class
+             (fn [class jg]
+               (.writeString jg (.toString class))))
+
+                                        ;(add-encoder compojure.core.)
+
 
 (defn json->edn [string]   (walk/keywordize-keys (json/parse-string string)))
 
