95b49088a3d34e4293aee808bc0f578b7882be89
Tyler Collins <tjscollins@gmail.com>
2018-04-12 10:47:50 -1000

    Fix inappropriate attributes in some components

--- src/cljs/webtools/components/buttons.cljs
+++ src/cljs/webtools/components/buttons.cljs
@@ -1,22 +1,30 @@
 (ns webtools.components.buttons
-  "Common, reusable button components"
-  )
+  "Common, reusable button components")
+
 
 (defn download-button [{:keys [text] :as opts-map}]
-  (let [default-opts {:role "button"}]
-    [:a.btn.btn-info.file-link (merge default-opts opts-map)
+  (let [default-opts {:role "button"}
+        opts (-> (merge default-opts opts-map)
+                 (dissoc :text))]
+    [:a.btn.btn-info.file-link opts
      [:i.fa.fa-download]
      " " text]))
 
 (defn edit-button [{:keys [title text] :as opts-map}]
-  (let [default-opts {:aria-label title}]
-    [:button.btn.btn-warning.file-link (merge default-opts opts-map)
+  (let [default-opts {:aria-label title}
+        opts (-> (merge default-opts opts-map)
+                 (dissoc :text)
+                 (dissoc :title))]
+    [:button.btn.btn-warning.file-link opts
      [:i.fa.fa-pencil]
      " " text]))
 
 (defn delete-button [{:keys [title text] :as opts-map}]
-  (let [default-opts {:aria-label title}]
-    [:button.btn.btn-danger.file-link (merge default-opts opts-map)
+  (let [default-opts {:aria-label title}
+        opts (-> (merge default-opts opts-map)
+                 (dissoc :text)
+                 (dissoc :title))]
+    [:button.btn.btn-danger.file-link opts
      [:i.fa.fa-trash]
      " " text]))
 
@@ -25,19 +33,27 @@
                    :as opts-map}]
   (let [default-opts {:type "submit"
                       :style {:width "100%"
-                              :height "100%"}}]
+                              :height "100%"}}
+        opts (-> (merge default-opts opts-map)
+                 (dissoc :text))]
     [:button#upload-btn.btn.btn-primary.form-control
-     (merge default-opts opts-map)
+     opts
      text]))
 
 (defn info-button  [{:keys [title text] :as opts-map}]
-  (let [default-opts {:role "button"}]
-    [:a.btn.btn-info.file-link (merge default-opts opts-map)
+  (let [default-opts {:role "button"}
+        opts (-> (merge default-opts opts-map)
+                 (dissoc :text)
+                 (dissoc :title))]
+    [:a.btn.btn-info.file-link opts
      [:i.fa.fa-info-circle]
      " " text]))
 
 (defn msg-button  [{:keys [title text] :as opts-map}]
-  (let [default-opts {:aria-label title}]
-    [:button.btn.btn-success.file-link (merge default-opts opts-map)
+  (let [default-opts {:aria-label title}
+        opts (-> (merge default-opts opts-map)
+                 (dissoc :text)
+                 (dissoc :title))]
+    [:button.btn.btn-success.file-link opts
      [:i.fa.fa-envelope]
      " " text]))
