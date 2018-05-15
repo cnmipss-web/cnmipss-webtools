73aa40cb33018f180ae76c4b55725b1201157751
Tyler Collins <tjscollins@gmail.com>
2018-05-14 17:09:05 -1000

    Fix reverted code from master

--- src/clj/webtools/email.clj
+++ src/clj/webtools/email.clj
@@ -2,14 +2,12 @@
   (:require [clojure.data :refer [diff]]
             [clojure.spec.alpha :as spec]
             [clojure.tools.logging :as log]
-            [hiccup.core :refer [html]]
             [postal.core :refer [send-message]]
             [webtools.db :as db]
             [webtools.email.templates :as templates]
             [webtools.exceptions :as w-ex]
             [webtools.models.procurement.core :as p]
-            [webtools.util :as util]
-            [webtools.util.dates :as util-dates]))
+            [webtools.util :as util]))
 
 (def ^:private webmaster-email "webmaster@cnmipss.org")
 (def ^:private procurement-email "procurement@cnmipss.org")
@@ -186,29 +184,16 @@
                                  (p/title-string pns)
                                  " has passed.")
                    :body [{:type "text/html"
-                           :content (html
-                                     [:html
-                                      [:body
-                                       [:p (str "Greetings " contact_person ",")]
-                                       [:p (str "We would like to notify you that the deadline to submit a response for "
-                                                (-> pns :type name clojure.string/upper-case) "# "
-                                                (:number pns)
-                                                " " (:title pns)
-                                                " has passed as of "
-                                                (util-dates/print-date-at-time (:close_date pns))
-                                                ".  No further submissions will be accepted after this time.")]
-                                       [:br]
-                                       [:p "If you have any questions, please contact Kimo Rosario at kimo.rosario@cnmipss.org"]
-                                       [:br]
-                                       [:p "Thank you,"]
-                                       [:p "Kimo Rosario"]
-                                       [:p "Procurement & Supply Officer"]
-                                       [:p "CNMI PSS"]
-                                       (templates/unsubscribe-option email :procurement)]])}]})
-    (catch Exception e
-      (log/error e))))
+                           :content (templates/notify-closed pns contact_person email)}]})
+    (catch Exception ex
+      (alert-error ex)
+      (log/error ex))))
 
-(defn notify-procurement [{:keys [company_name proc_id contact_person email telephone] :as sub} pns]
+(spec/fdef notify-pns-closed
+           :args (spec/cat :pns :webtools.spec.procurement/record
+                           :sub :webtools.spec.subscription/record))
+
+(defn notify-procurement [{:keys [company_name proc_id] :as sub} pns]
   (doseq [user (db/get-proc-users)]
     (let [tstr (p/title-string pns)]
       (try
@@ -216,15 +201,11 @@
                        :from "no-reply@cnmipss.org"
                        :subject (str company_name " has requested information regarding " tstr)
                        :body [{:type "text/html"
-                               :content (html
-                                         [:html
-                                          [:body
-                                           [:p (str contact_person " at " company_name
-                                                    " has subscribed to receive more information regarding "
-                                                    tstr)]
-                                           [:p (str "They may be contact by email at " email
-                                                    " or by telephone at " (util/format-tel-num telephone))]
-                                           [:p (str "They will be automatically notified by email regarding any changes or updates made to "
-                                                    tstr " via CNMI PSS Webtools.")]]])}]})
-        (catch Exception e
-          (log/error e))))))
+                               :content (templates/new-subscription-request sub pns)}]})
+        (catch Exception ex
+          (alert-error ex)
+          (log/error ex))))))
+
+(spec/fdef notify-procurement
+           :args (spec/cat :sub :webtools.spec.subscription/record
+                           :pns :webtools.spec.procurement/record))
